5392960d4823681c269788b890af863151aeba77
Maicol C Rodrigo <maicolcrodrigoa@gmail.com>
2023-01-04 09:00:50 -0500

    feat: init nuxt app

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+  </div>
+  </div>
+</template>
---
+++ components/AppAlert.vue
@@ -0,0 +1,10 @@
+<template>
+  <span>
+    <slot />
+    {{counter}}
+  </span>
+</template>
+
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
